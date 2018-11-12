00e4f0039e211bb94c6a5e5b0734a6d3f67a6104
absenthecon <33213855+absenthecon@users.noreply.github.com>
2018-11-12 16:36:28 +1100

    A Horrendously programmed timeclock program

---
+++ TimeKeeper/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.25!)
+        Me.Label1.Location = New System.Drawing.Point(388, 177)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(303, 27)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Date and Time should be here"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(383, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(323, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Batescrew Pumps and Valves"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!)
+        Me.Button1.Location = New System.Drawing.Point(428, 416)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(254, 63)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "CLOCK IN"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(428, 231)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(254, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Tag = ""
+        Me.TextBox1.UseSystemPasswordChar = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(524, 215)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "User ID:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(524, 254)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Job No."
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(428, 270)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(254, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(524, 293)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Notes:"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(428, 309)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(254, 78)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(510, 393)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(70, 17)
+        Me.RadioButton1.TabIndex = 4
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Clock out"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.Button1
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1264, 749)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "TimeKeepr"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents RadioButton1 As RadioButton
+End Class
---
+++ TimeKeeper/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Net.Mail
+
+Public Class Form1
+    Dim WithEvents aTimer As New System.Windows.Forms.Timer 'could have been done in the Designer
+
+    Private Sub aTimer_Tick(ByVal sender As Object,
+                            ByVal e As System.EventArgs) Handles aTimer.Tick
+        Label1.Text = DateTime.Now.ToString("dd MMMM, yyyy h:mm:ss tt")
+    End Sub
+
+    Private Sub Form1_Shown(ByVal sender As Object,
+                            ByVal e As System.EventArgs) Handles Me.Shown
+        aTimer.Interval = 250
+        aTimer.Start()
+        TextBox1.Select()
+        If Date.Now.Hour > 12 Then
+            Button1.Text = "CLOCK OUT"
+            RadioButton1.Text = "CLOCK IN"
+        Else
+            Button1.Text = "CLOCK IN"
+            RadioButton1.Text = "CLOCK OUT"
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+
+        Try
+            Dim Smtp_Server As New SmtpClient
+            Dim e_mail As New MailMessage()
+            Smtp_Server.UseDefaultCredentials = False
+            Smtp_Server.Credentials = New Net.NetworkCredential("timekeepr@test.internal", "password")
+            Smtp_Server.Port = 25
+            Smtp_Server.EnableSsl = False
+            Smtp_Server.Host = "127.0.0.1"
+
+            e_mail = New MailMessage()
+            e_mail.From = New MailAddress("TimeKeepr@test.internal")
+            e_mail.To.Add("timekeepr@test.internal")
+            If Button1.Text = "CLOCK IN" Then
+                e_mail.Subject = "User " & TextBox1.Text & " clocked in"
+                e_mail.IsBodyHtml = False
+                e_mail.Body = TextBox1.Text & " > " & DateTime.Now.ToString("dd/mm/yyyy HH:mm:ss") & " > " & TextBox2.Text & " > " & RichTextBox1.Text
+                Smtp_Server.Send(e_mail)
+                MsgBox("Clocked in")
+            Else
+                e_mail.Subject = "User " & TextBox1.Text & " clocked out"
+                e_mail.IsBodyHtml = False
+                e_mail.Body = TextBox1.Text & " > " & DateTime.Now.ToString("dd/mm/yyyy HH:mm:ss") & " > " & TextBox2.Text & " > " & RichTextBox1.Text
+                Smtp_Server.Send(e_mail)
+                MsgBox("Clocked out")
+            End If
+        Catch error_t As Exception
+            MsgBox(error_t.ToString)
+        End Try
+
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        RichTextBox1.Text = ""
+        RadioButton1.Checked = False
+        Button1.Text = "CLOCK IN"
+        TextBox1.Select()
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        If Button1.Text = "CLOCK OUT" Then
+            Button1.Text = "CLOCK IN"
+        Else
+            Button1.Text = "CLOCK OUT"
+        End If
+    End Sub
+End Class
